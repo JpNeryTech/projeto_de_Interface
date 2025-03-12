83c125a84a3a5098207de1aebe118e237a07f389
JpNery_Tech <jppereiranery@gmail.com>
2025-03-12 18:07:57 -0300

    Add files via upload

---
+++ Atividade1/script.js
@@ -0,0 +1,3 @@
+//criando um alerta
+var mensagem = prompt("Olá sejam bem vindos(as) ao nosso site: crescer sem limite! Por favor digite seu nome:")
+var mensagem2 = prompt("Muito bem! Agora digite de onde você está acessando este site:")
