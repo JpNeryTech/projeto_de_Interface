a956e62d499424add9ff680a2e54af9ae3133507
João Pedro Nery <jppereiranery@gmail.com>
2025-05-26 13:13:52 -0300

    Atualizando atividades

--- Atividade1/script.js
+++
@@ -1,3 +0,0 @@
-//criando um alerta
-var mensagem = prompt("Olá sejam bem vindos(as) ao nosso site: crescer sem limite! Por favor digite seu nome:")
-var mensagem2 = prompt("Muito bem! Agora digite de onde você está acessando este site:")
